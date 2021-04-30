58e9d655735b9ad78fd306e4eaeccb06bcbb044a
Ludmilishee <luda_babich@mail.ru>
2021-05-01 00:54:59 +0500

    task-19: fix merge

--- src/ClientApp/src/components/Person/index.jsx
+++ src/ClientApp/src/components/Person/index.jsx
@@ -11,9 +11,6 @@ export default function Person({person, onClick, offsets, isGodMode}) {
   let x = (person.position.x / MAX_WIDTH) * 100;
   let y = (person.position.y / MAX_HEIGHT) * 100;
 
-export default function Person({ person, onClick }) {
-  const x = (person.position.x / MAX_WIDTH) * 100;
-  const y = (person.position.y / MAX_HEIGHT) * 100;
   const clickHandle = (e) => {
     e.stopPropagation();
     onClick(person.id)
@@ -22,23 +19,22 @@ export default function Person({ person, onClick }) {
  function getClassName(isInfected, isBoring, hasImmunity) {
     let className = `${styles.root}`
     if (isInfected && isBoring) {
-      className += ` ${styles.boredAndInfected}`
+      className += ` ${styles.boredAndInfected}`;
       return className;
     }
     if (isInfected) {
-      className += ` ${styles.infected}`
+      className += ` ${styles.infected}`;
     }
     if (isBoring) {
-      className += ` ${styles.bored}`
+      className += ` ${styles.bored}`;
     }
 
     if (hasImmunity) {
       className += ` ${styles.immunity}`
     }
-    return className
-
-
+    return className;
   }
+
   const onDragEnd = (e) => {
     if (isGodMode && offsets && offsets.length) {
       const [offsetX, offsetY, width, height] = offsets;
@@ -55,25 +51,25 @@ export default function Person({ person, onClick }) {
 
   const setNewPersonPosition = (person) => {
     fetch(`${personPositionUrl}${person.id}`,
-        {
-          method: 'PATCH',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(person.position)
-        })
-        .then(errorHandler)
-        .then(() => person.isFrozen = false)
+      {
+        method: 'PATCH',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(person.position)
+      })
+      .then(errorHandler)
+      .then(() => person.isFrozen = false)
   };
 
   return (
     <div
-        className = {getClassName(person.infected, person.isBoring, person.hasImmunity)}
-        draggable={isGodMode}
-        onDragEnd={onDragEnd}
-        style={{left: `${x}%`, top: `${y}%`}}
-        onClick={clickHandle}
+      className = {getClassName(person.infected, person.isBoring, person.hasImmunity)}
+      draggable={isGodMode}
+      onDragEnd={onDragEnd}
+      style={{left: `${x}%`, top: `${y}%`}}
+      onClick={clickHandle}
     />
   );
 }
