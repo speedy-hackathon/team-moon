8106f062c11e3bdb5e3d0ee9be984ef6cec7ea13
Folleach <32067915+Folleach@users.noreply.github.com>
2021-04-30 21:02:03 +0500

    Create PersonController
    - Path for change person position

---
+++ src/Controllers/PersonController.cs
@@ -0,0 +1,21 @@
+﻿using covidSim.Models;
+using covidSim.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace covidSim.Controllers
+{
+    [Route("api/person")]
+    public class PersonController : Controller
+    {
+        [HttpPatch("{id}")]
+        public IActionResult Patch([FromRoute] int id, [FromBody] Vec position)
+        {
+            var game = Game.Instance;
+            var person = game.People.Find(p => p.Id == id);
+            if (person == null)
+                return NoContent();
+            person.Position = position;
+            return NoContent();
+        }
+    }
+}
