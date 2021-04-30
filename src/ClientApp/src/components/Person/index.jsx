7e60babdbf44c88e2c2550f10785224ba172d5ed
BigBird15 <justabigbird@gmail.com>
2021-05-01 01:29:13 +0500

    рефакторинг

--- src/ClientApp/src/components/Person/index.jsx
+++ src/ClientApp/src/components/Person/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./styles.module.css";
 import { MAX_HEIGHT, MAX_WIDTH } from "../../consts/sizes";
 
-export default function Person({ person, onClick }) {
 
   class Border {
     constructor(id) {
@@ -19,6 +18,8 @@ export default function Person({ person, onClick }) {
       return `${this.borderThickness}px ${this.borderTypes[this.borderTypeIndex]} rgb(${this.red},${this.green},${this.blue})`
     }
   }
+
+export default function Person({ person, onClick }) {
 
   const x = (person.position.x / MAX_WIDTH) * 100;
   const y = (person.position.y / MAX_HEIGHT) * 100;
