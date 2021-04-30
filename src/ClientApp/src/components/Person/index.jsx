b469d578fa5aa39b7250f26ad1d53533a08f08f6
BigBird15 <justabigbird@gmail.com>
2021-05-01 00:45:00 +0500

    Люди индивидуальны

--- src/ClientApp/src/components/Person/index.jsx
+++ src/ClientApp/src/components/Person/index.jsx
@@ -5,6 +5,17 @@ import { MAX_HEIGHT, MAX_WIDTH } from "../../consts/sizes";
 export default function Person({ person, onClick }) {
   const x = (person.position.x / MAX_WIDTH) * 100;
   const y = (person.position.y / MAX_HEIGHT) * 100;
+
+  //const red = Math.floor(Math.random() * 256);
+  const red = person.id * 10 % 256; 
+  const green = person.id * 20 % 256;
+  const blue = person.id * 80 % 256;
+
+
+  const borderThickness = person.id % 2 + 1;
+  const borderTypeIndex = person.id % 3;
+  const borderTypes = ['solid', 'dotted', 'dashed']
+
   function getClassName(isInfected, isBoring) {
     let className = `${styles.root}`
     if (isInfected && isBoring) {
@@ -18,13 +29,11 @@ export default function Person({ person, onClick }) {
       className += ` ${styles.bored}`
     }
     return className
-    
-
   }
   return (
     <div
       className = {getClassName(person.infected, person.isBoring)}
-      style={{ left: `${x}%`, top: `${y}%` }}
+      style={{ left: `${x}%`, top: `${y}%`, border:`${borderThickness}px ${borderTypes[borderTypeIndex]} rgb(${red},${green},${blue})` }}
       onClick={() => onClick(person.id)}
     />
   );
