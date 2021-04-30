45f42c1a255acd5e1d9b496318113dde618d2a6d
AppleePie <64743297+AppleePie@users.noreply.github.com>
2021-05-01 01:11:50 +0500

    Добавили класс immunity для окрашивания персонажа с иммунитетом

--- src/ClientApp/src/components/Person/index.jsx
+++ src/ClientApp/src/components/Person/index.jsx
@@ -5,7 +5,7 @@ import { MAX_HEIGHT, MAX_WIDTH } from "../../consts/sizes";
 export default function Person({ person, onClick }) {
   const x = (person.position.x / MAX_WIDTH) * 100;
   const y = (person.position.y / MAX_HEIGHT) * 100;
-  function getClassName(isInfected, isBoring) {
+  function getClassName(isInfected, isBoring, hasImmunity) {
     let className = `${styles.root}`
     if (isInfected && isBoring) {
       className += ` ${styles.boredAndInfected}`
@@ -17,13 +17,17 @@ export default function Person({ person, onClick }) {
     if (isBoring) {
       className += ` ${styles.bored}`
     }
+    
+    if (hasImmunity) {
+      className += ` ${styles.immunity}`
+    }
     return className
     
 
   }
   return (
     <div
-      className = {getClassName(person.infected, person.isBoring)}
+      className = {getClassName(person.infected, person.isBoring, person.hasImmunity)}
       style={{ left: `${x}%`, top: `${y}%` }}
       onClick={() => onClick(person.id)}
     />
