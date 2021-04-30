88b839243421937a908228771d9e7cf9a3cad677
AppleePie <64743297+AppleePie@users.noreply.github.com>
2021-05-01 01:30:46 +0500

    Фикс task17

--- src/ClientApp/src/components/PersonPath/index.jsx
+++ src/ClientApp/src/components/PersonPath/index.jsx
@@ -1,7 +1,12 @@
 import styles from "./style.module.css";
+import {MAX_WIDTH} from "../../consts/sizes";
 
 export const PersonPath = ({path}) => {
-    const pathToString = (path) => path.map(({x, y}) => `${x},${y}`).join(' ')
+    const pathToString = (path) => path.map(({x, y}) => {
+        const x1 = (x / MAX_WIDTH) * 100;
+        const y1 = (y / MAX_WIDTH) * 100
+        return `${x1},${y1}`
+    }).join(' ')
 
     return (
         <svg className={styles.svg}>
