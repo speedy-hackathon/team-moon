8ff6a6765ac132885e10737669d6a1c3367262fc
vabyars <vasiliyrait@gmail.com>
2021-04-30 20:47:28 +0500

    Remove person state colors

--- src/ClientApp/src/components/Person/index.jsx
+++ src/ClientApp/src/components/Person/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./styles.module.css";
 import { MAX_HEIGHT, MAX_WIDTH } from "../../consts/sizes";
-import { AT_HOME, GOING_HOME, WALKING } from "../../consts/playerStates";
 
 
 export default function Person({ person, onClick }) {
@@ -9,23 +8,9 @@ export default function Person({ person, onClick }) {
   const y = (person.position.y / MAX_HEIGHT) * 100;
   return (
     <div
-      className={`${styles.root} ${getStateStyle(person.state)}`}
+      className={styles.root}
       style={{ left: `${x}%`, top: `${y}%` }}
       onClick={() => onClick(person.id)}
     />
   );
-}
-
-
-function getStateStyle(state){
-  switch(state){
-    case AT_HOME:
-      return styles.atHome
-    case WALKING:
-      return styles.walking
-    case GOING_HOME:
-      return styles.goingHome
-    default:
-      return ''
-  }
 }
--- src/ClientApp/src/consts/playerStates.js
+++
@@ -1,3 +0,0 @@
-export const AT_HOME = 'AtHome'
-export const WALKING = 'Walking'
-export const GOING_HOME = 'GoingHome'
